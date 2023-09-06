f1a836e84e83ff8ee74b492bf49221a18b822286
Pranjal Sharma <143706871+pranjal-uh@users.noreply.github.com>
2023-09-06 16:08:59 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -67,6 +67,7 @@ function displayBlipCaption(response) {
 
     // Check if the response contains outputs
     console.log(response);
+    console.log(response.outputs);
     if (response && response.outputs && response.outputs.length > 0) {
         // Extract the raw caption text from the response
         const captionText = response.outputs[0].data.text.raw;
